4a2f8a7c958553c201e92829dff524149798b0d9
Evgenyi_Malyutin <Evgen986@mail.run>
2022-09-30 22:14:04 +0300

    task3

--- Seminar_1/task1/Program.cs
+++ Seminar_1/task1/Program.cs
@@ -2,9 +2,12 @@
 int number1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = int.Parse(Console.ReadLine());
-int number3 = number1/number2;
-if(number3==number2){
+int number3 = number1 / number2;
+if (number3 == number2)
+{
     Console.WriteLine("Число - " + number1 + " является квадратом числа - " + number2);
-}else{
+}
+else
+{
     Console.WriteLine("Число - " + number1 + " не является квадратом числа - " + number2);
 }
---
+++ Seminar_1/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int a = number * -1;
+if (number < 0)
+{
+    while (number < a)
+    {
+        Console.Write(number);
+        number++;
+    }
+}
+else
+{
+    while (a < number)
+    {
+        Console.Write(a);
+        a++;
+    }
+}
