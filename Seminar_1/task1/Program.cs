bbfb1d567ed85817cddbbd830afbb96f89bc7e0b
Evgenyi_Malyutin <Evgen986@mail.run>
2022-09-30 20:47:06 +0300

    task1

---
+++ Seminar_1/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+int number3 = number1/number2;
+if(number3==number2){
+    Console.WriteLine("Число - " + number1 + " является квадратом числа - " + number2);
+}else{
+    Console.WriteLine("Число - " + number1 + " не является квадратом числа - " + number2);
+}
