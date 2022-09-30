32223d9c55429b7d7707bb41a6eefaac26ce8b8e
Evgenyi_Malyutin <Evgen986@mail.run>
2022-09-30 22:19:45 +0300

    task4

---
+++ Seminar_1/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100 || number > 1000)
+{
+    Console.WriteLine("Введите корректное число!");
+}
+else
+{
+    int a = number % 10;
+    Console.WriteLine(a);
+}
