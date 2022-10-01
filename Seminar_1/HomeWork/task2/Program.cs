704e00243ae65645e2c377aaa77584ee288579a7
Evgen986 <Evgen986@mail.ru>
2022-10-01 16:03:00 +0300

    task2_1

--- Seminar_1/HomeWork/task2/Program.cs
+++ Seminar_1/HomeWork/task2/Program.cs
@@ -3,7 +3,10 @@ int number1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = int.Parse(Console.ReadLine());
 int max = number1;
+int min = number2;
 if (number2>max){
     max=number2;
+    min=number1;
 }
 Console.WriteLine("Большее число: " + max);
+Console.WriteLine("Меньшее число: " + min);
