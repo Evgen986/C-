e6d4bd02209a4d6c96409302c7c1bb318705d7fd
Evgenyi_Malyutin <Evgen986@mail.run>
2022-10-08 11:56:59 +0300

    Seminar + HomeWork

---
+++ Seminar_3/task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 21: Напишите программу, которая принимает на вход координаты двух точек 
+              и находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21
+*/
+Console.Clear();
+Console.WriteLine("Введите координату X точки №1: ");
+double point1X = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки №1: ");
+double point1Y = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату X точки №2: ");
+double point2X = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки №2: ");
+double point2Y = int.Parse(Console.ReadLine());
+double distance = Math.Sqrt(Math.Pow(point2X-point1X, 2)+Math.Pow(point2Y-point1Y, 2));
+Console.WriteLine("Дистанция = " + Math.Round(distance, 2));
---
+++ Seminar_3/task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+             5 -> 1, 4, 9, 16, 25.
+            2 -> 1,4
+
+*/
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write(number + " -> ");
+for (int i = 1; i <= number; i++)
+{
+    double temp = Math.Pow(i, 2);
+    Console.Write(temp + ", ");
+}
