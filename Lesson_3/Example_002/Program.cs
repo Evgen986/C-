ea21defc1ad24794ebce399551db03a23e2bcf5a
Evgenyi_Malyutin <Evgen986@mail.run>
2022-10-09 14:59:20 +0300

    Lesson_3

---
+++ Lesson_3/Example_002/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Напишите метод который будет отсортировывать массив от минимального к максимальному
+*/
+int[] array = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+int[] sortarray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = i+1; j < length; j++)
+        {
+            int temp = 0;
+            if (array[j] < array[i])
+            {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    return array;
+}
+void printArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[ ");
+    for (int i = 0; i < count; i++)
+    {
+        if (i == count - 1)
+        {
+            Console.Write("]");
+        }
+        else
+            Console.Write(array[i] + ", ");
+    }
+}
+int[] temp = sortarray(array);
+printArray(temp);
---
+++ Lesson_3/Example_003/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Напишите метод который будет отсортировывать массив от макисмального к минимальному
+*/
+int[] array = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void printArray(int [] array){
+    Console.Write("[ ");
+    for (int i = 0; i<array.Length; i++){
+        if(i == array.Length-1){
+        Console.Write(array[i] + " ]");    
+        }else
+        Console.Write( array[i] + ", ");
+    }
+}
+
+void SortArrayMax (int [] array){
+    for(int i =0; i<array.Length -1; i++){
+        int maximal = i;
+            for(int j=i+1; j<array.Length; j++)
+            {
+                if(array[j]>array[maximal]){
+                    maximal = j;
+                }
+            }
+        int temp = array[i];
+        array[i] = array[maximal];
+        array[maximal] = temp;
+    }
+}
+SortArrayMax(array);
+printArray(array);
