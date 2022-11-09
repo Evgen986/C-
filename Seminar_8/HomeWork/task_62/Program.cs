9bc4b428d6d56231a578cd1a0ab7e608db95d5aa
Evgen986 <Evgen986@mail.ru>
2022-11-09 19:38:43 +0300

    task_62

--- Seminar_8/HomeWork/task_62/Program.cs
+++ Seminar_8/HomeWork/task_62/Program.cs
@@ -8,62 +8,64 @@ void Main()
     int[,] array = new int[4, 4];
     FillSpiralArray(array);
     PrintArray(array);
-
-
 }
 
 void FillSpiralArray(int[,] array)
 {
-    int count = 1;
-    for (int i = 0; i < array.GetLength(1); i++)
+    int count = 1;                                              // Счетчик для заполнения массива
+    // Заполняем периметр массива                                                        
+    for (int i = 0; i < array.GetLength(1); i++)                // Двигаемся в право по первой строке
     {
         array[0, i] = count;
         count++;
     }
-    for (int j = 1; j < array.GetLength(0); j++)
+    for (int j = 1; j < array.GetLength(0); j++)                // Двигаемся вниз по крайней правой колнке
     {
         array[j, array.GetLength(1) - 1] = count;
         count++;
     }
-    for (int i = array.GetLength(1) - 2; i >= 0; i--)
+    for (int i = array.GetLength(1) - 2; i >= 0; i--)           // Двигаемся в лево по нижней строке
     {
         array[array.GetLength(0) - 1, i] = count;
         count++;
     }
-    for (int j = array.GetLength(0) - 2; j > 0; j--)
+    for (int j = array.GetLength(0) - 2; j > 0; j--)            // Двигаемся вверх по крайней левой колонке до значения 1
     {
         array[j, 0] = count;
         count++;
     }
-    int coordRows = 1;
+    // Определяем точку для заполнения массива внутри периметра
+    int coordRows = 1;                  
     int coordColumns = 1;
-    while (count < array.GetLength(0) * array.GetLength(1))
+    // Начинаем заполнять массив внутри периметра
+    while (count < array.GetLength(0) * array.GetLength(1))     // Цикл будет заполнять массив пока count-1 меньше чем количество ячеек масива
     {
-        while (array[coordRows, coordColumns + 1] == 0)
+        while (array[coordRows, coordColumns + 1] == 0)         // Пока в ячейке справа значение равно 0
         {
             array[coordRows, coordColumns] = count;
             count++;
             coordColumns++;
         }
-        while (array[coordRows + 1, coordColumns] == 0)
+        while (array[coordRows + 1, coordColumns] == 0)         // Пока в ячейке снизу значение равно 0
         {
             array[coordRows, coordColumns] = count;
             count++;
             coordRows++;
         }
-        while (array[coordRows, coordColumns - 1] == 0)
+        while (array[coordRows, coordColumns - 1] == 0)         // Пока в ячейке слева значение равно 0
         {
             array[coordRows, coordColumns] = count;
             count++;
             coordColumns--;
         }
-        while (array[coordRows - 1, coordColumns] == 0)
+        while (array[coordRows - 1, coordColumns] == 0)         // Пока в ячейке сверху значение равно 0
         {
             array[coordRows, coordColumns] = count;
             count++;
             coordRows--;
         }
     }
+    // Заполняем последнюю пустую ячейку
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
