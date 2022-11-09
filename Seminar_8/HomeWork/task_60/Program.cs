8ae5df1f7cf74cb89dce6a12f204c41d73313f92
Evgen986 <Evgen986@mail.ru>
2022-11-09 21:20:26 +0300

    task_60

--- Seminar_8/HomeWork/task_60/Program.cs
+++ Seminar_8/HomeWork/task_60/Program.cs
@@ -7,7 +7,7 @@ Main();
 void Main()
 {
     Console.Clear();
-    int[,,] array = GetArray(2, 2, 2);
+    int[,,] array = GetArray(3, 3, 3);
     Console.WriteLine("Массив со случайными значениями:");
     PrintArray(array);
 
@@ -16,7 +16,7 @@ void Main()
 int[,,] GetArray(int sizeArray1, int sizeArray2, int sizeArray3)
 {
     int[,,] array = new int[sizeArray1, sizeArray2, sizeArray3];
-    int count = 100;
+    int count = 99;
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
@@ -33,24 +33,13 @@ int[,,] GetArray(int sizeArray1, int sizeArray2, int sizeArray3)
 
 void PrintArray(int[,,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j, 0]}({i},{j},0) ");
-        }
-        Console.WriteLine();
-    }
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            for (int k = 1; k < array.GetLength(2); k++)
-            {
-                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+    for (int i =0; i<array.GetLength(0); i++){
+        for (int j=0; j<array.GetLength(1); j++){
+            for (int k=0; k<array.GetLength(2); k++){
+                Console.Write($"{array[j,k,i]} ({j},{k},{i}) ");
             }
+            Console.WriteLine();    
         }
         Console.WriteLine();
     }
-    Console.WriteLine();
 }
